d50d45434e48304a5eafeb0acbcb6cca35795f97
fuga-naito23 <naitofuga23@gmail.com>
2021-04-09 16:30:31 +0900

    modal create

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Header } from "../../components/header";
+import { Select } from "../../components/select";
+import { ToDoList } from "../../components/toDoList";
+import { FormModal } from "../../components/modal";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import "./style.css";
 
 export const Home = () => {
-    return <div className="main"></div>;
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="main">
+      <Header />
+      <div className="genre">
+        <Select />
+        <AddCircleOutlineIcon
+          className="add_circle_outline_icon"
+          fontSize="default"
+          onClick={handleOpen}
+        />
+        <FormModal handleClose={handleClose} isOpen={isOpen} />
+      </div>
+      <div className="contents">
+        <ToDoList />
+      </div>
+    </div>
+  );
+};
